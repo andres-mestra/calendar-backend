b401d7d5b0ca93f6b20fdb58240e70007fbba3b4
Andres Mestra <andresmestra11a@gmail.com>
2021-01-29 22:55:30 -0500

    inicialización de servidor

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import express from 'express'
+import express from 'express';
+
+
+const app = express();
+
+
+//Directorio publico
+app.use( express.static('public') );
+
+
+
+app.listen(4000, () => {
+  console.log('Server en http://localhost:4000');
+})
