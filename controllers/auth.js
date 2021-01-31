a2e53f27f96bfa7670f3798488410f6b6fee4dde
Andres Mestra <andresmestra11a@gmail.com>
2021-01-30 22:04:35 -0500

    Validar existencia de usuario y encriptando password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,35 @@
+import argon2 from 'argon2';
 import { response } from 'express';
 import { Usuario } from '../models/Usuario_model';
 
 export const crearUsuario = async (req, res = response) => {
 
-  //const { name, email, password } = req.body
+  const { email, password } = req.body
   try {
 
-    const usuario = new Usuario(req.body)
+    let usuario = await Usuario.findOne({ email });
+    if( usuario ){
+      return res.status(400).json({
+        ok: false,
+        msg: 'Un usuario existe con este email'
+      })
+    }
+
+    usuario = new Usuario(req.body);
+    //Encriptar contraseña
+    usuario.password = await argon2.hash( password )
+    //Guardar usuario 
     await usuario.save();
 
-    res.status(201).json({
+    return res.status(201).json({
       ok: true,
-      msg: 'registro',
+      uid: usuario.id,
+      name: usuario.name,
     })
 
   } catch (error) {
     console.log(error)
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       msg: 'Por favor hable con el administrador',
     })
